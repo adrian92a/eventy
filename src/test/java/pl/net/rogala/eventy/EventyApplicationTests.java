0f3c9cb576b7c303648721923d5fde0b9f791ce3
LAPTOP-36SKAH06\marla <marlenalukomska@wp.pl>
2019-03-10 17:34:11 +0100

    Initial commit

---
+++ src/test/java/pl/net/rogala/eventy/EventyApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.net.rogala.eventy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

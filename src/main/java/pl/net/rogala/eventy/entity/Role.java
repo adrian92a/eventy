9d2264dae8fc4c40e938cd26217a007a689bb2d8
szymanskimichal90 <szymanskimichal90@gmail.com>
2019-03-16 13:42:39 +0100

    Creating new branch 'comments'

--- src/main/java/pl/net/rogala/eventy/entity/Role.java
+++ src/main/java/pl/net/rogala/eventy/entity/Role.java
@@ -1,6 +1,7 @@
 package pl.net.rogala.eventy.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,7 +9,10 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Role {
+
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

e952957eb0c6c944e9cb5ee8ce23969a9a93c8be
adrian92a <adrian.skorupa92@gmail.com>
2019-03-16 15:20:21 +0100

    Adrian test

--- src/main/java/pl/net/rogala/eventy/controller/EventController.java
+++ src/main/java/pl/net/rogala/eventy/controller/EventController.java
@@ -6,29 +6,41 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.net.rogala.eventy.entity.Event;
+import pl.net.rogala.eventy.repository.EventRepository;
 import pl.net.rogala.eventy.service.EventService;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
 public class EventController {
 
     private EventService eventService;
-
+    private EventRepository eventRepository;
     @Autowired
-    public EventController(EventService eventService) {
+    public EventController(EventService eventService, EventRepository eventRepository) {
         this.eventService = eventService;
+        this.eventRepository = eventRepository;
     }
 
     @GetMapping("/findEvents")
     public String showMainPage(@RequestParam("name") String name,
                                @RequestParam("option") String option,
                                Model model) {
-
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
         List<Event> eventList= eventService.showEventList();
+        switch (option){
+            case "przyszłe":eventList= eventRepository.findFutureEvents(name,localDate);
+            break;
+            case "trwające i przyszłe":eventList=eventRepository.findFutureAndActuallyEvents(name,localDate);
+            break;
+            case "wszystkie":eventList=eventRepository.findEvents(name,localDate);
+        }
+
         model.addAttribute("eventList",eventList);
-        return "home";
+        return "home";  
     }
 
 }
--- src/main/java/pl/net/rogala/eventy/repository/EventRepository.java
+++ src/main/java/pl/net/rogala/eventy/repository/EventRepository.java
@@ -2,13 +2,24 @@ package pl.net.rogala.eventy.repository;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.net.rogala.eventy.entity.Event;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
+    @Query("Select e from Event e where e.name like '%?1%' and startDate>?2")
+    List<Event> findFutureEvents(String name, LocalDate date);
 
+    @Query("Select e from Event e where e.name like '%?1%' and startDate>=?2")
+    List<Event> findFutureAndActuallyEvents(String name, LocalDate date);
+
+    @Query("Select e from Event e where e.name like '%?1%'")
+    List<Event> findEvents(String name, LocalDate date);
 }
